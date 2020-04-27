64aa57e76bd262be10e56f617faa61fd3cc06a5d
JediMaster900 <charliemendes@msn.com>
2020-04-27 11:23:03 -0400

    most and least of confirmed and recovered cases

--- src/app/RowObjects/DeathData.java
+++ src/app/RowObjects/DeathData.java
@@ -19,6 +19,71 @@ public class DeathData {
         deaths = Integer.parseInt(data[10]);
     }
 
+    public static DeathData[] sortByConfirmed(DeathData[] deathData, boolean lowToHigh){
+            DeathData temp;
+            if(lowToHigh){
+                for (int i = 0; i < deathData.length - 1; i++ ){
+                    //initialize the smallest_index    
+                    int smallest_index = i;
+                    //inner loop locate smallest 
+                    for(int j = i + 1; j < deathData.length; j++) {
+                            if(deathData[ j ].confirmed() < deathData[ smallest_index ].confirmed())
+                                    smallest_index = j;
+                    }
+                    // 3 part swap between loops   
+                    temp = deathData[ smallest_index ];
+                    deathData[ smallest_index ] = deathData[ i ];
+                    deathData[ i ] = temp;
+                }// close the otter loop
+            }
+            
+        return deathData;
+    }
+
+    public static DeathData[] sortByRecovered(DeathData[] deathData, boolean lowToHigh){
+        DeathData temp;
+        if(lowToHigh){
+            for (int i = 0; i < deathData.length - 1; i++ ){
+                //initialize the smallest_index    
+                int smallest_index = i;
+                //inner loop locate smallest 
+                for(int j = i + 1; j < deathData.length; j++) {
+                        if(deathData[ j ].recovered() < deathData[ smallest_index ].recovered())
+                                smallest_index = j;
+                }
+                // 3 part swap between loops   
+                temp = deathData[ smallest_index ];
+                deathData[ smallest_index ] = deathData[ i ];
+                deathData[ i ] = temp;
+            }// close the otter loop
+        }
+        
+    return deathData;
+}
+
+    // -----------------
+    // ACCESSOR METHODS
+    // -----------------
+    public String state(){
+        return state;
+    }
+
+    public String country(){
+        return country;
+    }
+
+    public String lastUpdate(){
+        return country;
+    }
+
+    public int recovered(){
+        return recovered;
+    }
+
+    public int confirmed(){
+        return confirmed;
+    }
+
     public int deaths(){
         return deaths;
     }
