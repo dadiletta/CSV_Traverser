fa2f9f1e8c656c12ad169ba7eaa8f4025f8eeb12
Ricky Roberts <roberts7411@gmail.com>
2020-04-29 10:35:49 -0400

    Added Sorting method, and now prints most and least confimred and recovered cases.

--- src/app/RowObjects/DeathData.java
+++ src/app/RowObjects/DeathData.java
@@ -19,6 +19,79 @@ public class DeathData {
         deaths = Integer.parseInt(data[10]);
     }
 
+    public static DeathData[] sortByConfirmed(DeathData[] deathData, boolean lowToHigh){
+        if(lowToHigh){
+            for(int x = 1; x < deathData.length; x++){
+                DeathData key = deathData[x];
+                int y = x-1;
+                while((y > -1) && (deathData[y].confirmed() > key.confirmed())){
+                    deathData[y+1] = deathData[y];
+                    y--;
+                }
+                deathData[y+1] = key;
+            }
+        }
+        else{
+            for(int x = 1; x < deathData.length; x++){
+                DeathData key = deathData[x];
+                int y = x-1;
+                while((y > -1) && (deathData[y].confirmed() < key.confirmed())){
+                    deathData[y+1] = deathData[y];
+                    y--;
+                }
+                deathData[y+1] = key;
+            }
+        }
+        return deathData;
+    }
+
+    public static DeathData[] sortByRecovered(DeathData[] deathData, boolean lowToHigh){
+        if(lowToHigh){
+            for(int x = 1; x < deathData.length; x++){
+                DeathData key = deathData[x];
+                int y = x-1;
+                while((y > -1) && (deathData[y].recovered() > key.recovered())){
+                    deathData[y+1] = deathData[y];
+                    y--;
+                }
+                deathData[y+1] = key;
+            }
+        }
+        else{
+            for(int x = 1; x < deathData.length; x++){
+                DeathData key = deathData[x];
+                int y = x-1;
+                while((y > -1) && (deathData[y].recovered() < key.recovered())){
+                    deathData[y+1] = deathData[y];
+                    y--;
+                }
+                deathData[y+1] = key;
+            }
+        }
+        return deathData;
+    }
+
+    // -----------------------
+    // --- ACCESOR METHODS ---
+    // -----------------------
+    public int id(){
+        return id();
+    }
+    public String state(){
+        return state;
+    }
+    public String country(){
+        return country;
+    }
+    public String lastUpdate(){
+        return lastUpdate;
+    }
+    public int confirmed(){
+        return confirmed;
+    }
+    public int recovered(){
+        return recovered;
+    }
     public int deaths(){
         return deaths;
     }
